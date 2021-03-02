7579cbe85940df682922da74894c7b26b83d6346
Kaique Lira <Kaiqueliira@gmail.com>
2021-03-02 12:16:49 -0300

    adicionado novos recursos

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-window.onscroll = function() {myFunction()};
-var navbar = document.querySelector(".header");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  } 
-
-
-
   function enviado(){
     alert("Sua Solicitação foi enviada com Sucesso!")
   }
